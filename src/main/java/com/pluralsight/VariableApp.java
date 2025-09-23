2b7809660929dc840e8ad8498fd7c290533edd52
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-09-23 15:09:55 -0400

    Initial commit. Declared variables for Step 1.

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        String color = "pink";
+        double YUU= 2025;
+        char initial = 'C';
+        boolean hasPet = true;
+        String catchphrase = ("It's probably fine");
+
+    }
+}
