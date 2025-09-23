f45bc2b9439c7d84137d8d759e89ad9dd70ffa4f
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-09-23 15:35:53 -0400

    Declared and initialized variables for step 2.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -10,11 +10,15 @@ public class VariableApp {
         boolean hasPet = true;
         String catchphrase = ("'It's probably fine'");
 
-        // Step 2
         System.out.println("My favorite color is " + color + " because its pretty.");
         System.out.println("I started the Year Up United App Dev track in " + YUU + ".");
         System.out.println("My middle initial is " + initial);
         System.out.println("It's " + hasPet + " I had a cat, but he lives with my younger sister now.");
         System.out.println("When something goes awry, I like to say " + catchphrase + ".");
+
+int daysInTheWeek = 7;
+double coffeePrice = 4.99;
+char favLetter = 'B';
+boolean isRaining = false;
     }
 }
