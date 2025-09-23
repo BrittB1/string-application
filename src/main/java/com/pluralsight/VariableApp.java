090315f1ad44d06bcad391c658a79fc0bb1df6c1
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-09-23 15:23:06 -0400

    Printed each variable with a descriptive message.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,11 +3,18 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args) {
 
+        // Step 1
         String color = "pink";
-        double YUU= 2025;
+        int YUU = 2025;
         char initial = 'C';
         boolean hasPet = true;
-        String catchphrase = ("It's probably fine");
+        String catchphrase = ("'It's probably fine'");
 
+        // Step 2
+        System.out.println("My favorite color is " + color + " because its pretty.");
+        System.out.println("I started the Year Up United App Dev track in " + YUU + ".");
+        System.out.println("My middle initial is " + initial);
+        System.out.println("It's " + hasPet + " I had a cat, but he lives with my younger sister now.");
+        System.out.println("When something goes awry, I like to say " + catchphrase + ".");
     }
 }
