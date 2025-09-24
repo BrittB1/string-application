8062518e32c97b59fcc7d7d03b819c513541c740
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-09-23 21:30:20 -0400

    Created variables related to purchased item and concatenated into 'receipt'

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+
+        String purchasedItem = " new laptop";
+        double price = 799.00;
+        int quantity = 1;
+
+        System.out.println("You bought " + quantity + purchasedItem + " for " + "$" + price);
+
+    }
+
+}
